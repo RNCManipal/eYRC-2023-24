4016b12730fc990c50dacd6e5ce0ba8bc60ce12b
tanay1910 <74229592+tanay1910@users.noreply.github.com>
2023-09-17 12:56:19 +0530

    Update dst_bw_points.py

--- Task 0/Codechef Contest/dst_bw_points.py
+++ Task 0/Codechef Contest/dst_bw_points.py
@@ -1,47 +1,11 @@
-'''
-# Team ID:          < 2414 >
-# Theme:            < Cosmo Logistic >
-# Author List:      < Aditya Wadgaonkar,Harikishanthini K,Sujan Adiga,Tanay Srivastava  >
-# Filename:         < dst_bw_points.py >
-# Functions:        < compute_distance >
-# Global variables: < None >
-'''
-
 def compute_distance(x1, y1, x2, y2):
 
     distance = (((x2-x1)**2)+((y2-y1)**2))**0.5
 
     return distance
 
-    '''
-Purpose:
----
-< To Calculate the distance between given twopoints(x,y coordinate) >
-
-Input Arguments:
----
-`< x1 >` :  [ < int > ]
-    < x coordinate of 1st point >
-
-`< y1 >` :  [ < int > ]
-    < y coordinate of 1st point >
-
-`< x2 >` :  [ < int > ]
-    < x coordinate of 2nd point >
-
-`< y2 >` :  [ < int > ]
-    < y coordinate of 2nd point >
-
-Returns:
----
-`< distance >` :  [ < int > ]
-    < returns the distace between given points >
-
-Example call:
----
-< compute_distance(a,b ,A ,B ) >
-'''
-
+    # Complete this function to return Euclidean distance and
+    # print the distance value with precision up to 2 decimal places
 
 
 # Main function
@@ -52,11 +16,11 @@ if __name__ == '__main__':
 
     for i in range(0,test_cases):
     # Write the code here to take the x1, y1, x2 and y2 values
-        x1 = int(input("enter x1: "))
-        x2 = int(input("enter x2: "))
-        y1 = int(input("enter y1: "))
-        y2 = int(input("enter y2: "))
+        l1 = list(map(int, input().split()))
+        x1=l1[0]
+        y1=l1[1]
+        x2=l1[2]
+        y2=l1[3]
         
         # Once you have all 4 values, call the compute_distance function to find Euclidean distance
-        print(round(compute_distance(x1, y1, x2, y2),2))
-        
+        print(f"Distance: {round(compute_distance(x1, y1, x2, y2),2)}")
