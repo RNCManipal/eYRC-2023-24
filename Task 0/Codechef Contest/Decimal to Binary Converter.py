926402a70ac1d0d4f30562c9360b698c6157ea68
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-09-17 16:30:23 +0530

    Update Decimal to Binary Converter.py

--- Task 0/Codechef Contest/Decimal to Binary Converter.py
+++ Task 0/Codechef Contest/Decimal to Binary Converter.py
@@ -1,4 +1,5 @@
 
+
 '''
 # Team ID:          < 2414 >
 # Theme:            < Cosmo Logistic >
@@ -31,25 +32,27 @@ def dec_to_binary(n):
     >>> print(binary)
     '00000101'
     """
-    if n == 0:
-        return '0' * 8
-    else:
-        binary_representation = dec_to_binary(n // 2)
-        return binary_representation[:-1] + str(n % 2)
 
+    bin_num = bin(n)[2:]
+    bin_num = bin_num.zfill(8)
+    return bin_num
 
 # Function Name:    main (built-in)
 # Inputs:           Input the Decimal number
 # Outputs:          Prints the binary 8bit format of decimal number
 # Purpose:          To call the  dec_to_binary() function to perform the desired conversion from decimal to binary and take the inputs in form of decimal number.
-if __name__ == "__main__":
-    # test_cases :Input total number of test cases
+if __name__ == '__main__':
+    # take the T (test_cases) input
     test_cases = int(input())
-    for _ in range(test_cases):
-        # Input the decimal number
+    list=[]*25
+
+    # Write the code here to take the n value
+    for case in range(test_cases):
+        # take the n input values
         n = int(input())
+        list.append(n)
+
         # Once you have the n value, call the dec_to_binary function to find the binary equivalent of 'n' in 8-bit format
-        # Convert to binary and format it to be 8 bits long
-        binary_representation = dec_to_binary(n).zfill(8)
-        #Output the result
-        print(binary_representation)
+    for a in range( test_cases ):
+        bin_num = dec_to_binary(list[a])
+        print(bin_num)
