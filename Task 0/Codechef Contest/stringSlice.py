bec531aa8b5ae8eb9bc94c758786d7db1c0fe3f1
tanay1910 <74229592+tanay1910@users.noreply.github.com>
2023-09-17 12:48:22 +0530

    Update stringSlice.py

--- Task 0/Codechef Contest/stringSlice.py
+++ Task 0/Codechef Contest/stringSlice.py
@@ -7,19 +7,36 @@
 # Global variables: < None >
 '''
 
-l1 = []
+test_cases = int(input())
 
-len = int(input("Length of list: "))
-for i in range(0,len):
-    a=int(input("Enter number: "))
-    l1.append(a)
+while test_cases !=0:
+    len1 = int((input()))
 
-print(l1[::-1])
+    l1 = list(map(int, input().split()))
+    len1 = len(l1)
+    l2 =l1[::-1]
+    for item in l2:
+        print(item,end=" ")
 
-print((l1[::2])+3)
+    print("\n")
+    for i in range(0, len1, 3):
+        if i == 0 :
+            continue
+        else:
+            print(l1[i]+3,end=" ")
+    print("\n")
+   
 
-print((l1[::5])-7)
+    for i in range(0, len1, 5):
+        if i == 0 :
+            continue
+        else:
+            print(l1[i]-7,end=" ")
+    print("\n")
 
-for i in range(3,8):
-    sum+=l1[i]
-print(sum)
+    sum=0
+    for i in range(3,8):
+        sum= sum+l1[i]
+    print(f"{sum}")
+
+    test_cases= test_cases-1
