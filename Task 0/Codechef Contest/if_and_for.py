a056504978c86ff3421d9c92b2406e7521bb46c0
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-09-17 14:00:27 +0530

    Update if_and_for.py

--- Task 0/Codechef Contest/if_and_for.py
+++ Task 0/Codechef Contest/if_and_for.py
@@ -3,19 +3,27 @@
 # Theme:            < Cosmo Logistic >
 # Author List:      < Aditya Wadgaonkar,Harikishanthini K,Sujan Adiga,Tanay Srivastava  >
 # Filename:         < if_and_for.py >
-# Functions:        < None >
+# Functions:        < main >
 # Global variables: < None >
 '''
-test_cases = int(input())
 
-for i in range(0,test_cases):
-    n = int(input())
-    for i in range(0,n):
-            if i!=0:
+# Function Name:    main (built in)
+#        Inputs:    Number of test cases and integers
+#       Outputs:    Numbers as output after applying conditions
+#       Purpose:    Print the square of i if i is odd or double the value if i is even and if i is zero then add 3
+if __name__ == '__main__':
+  test_cases = int(input())
+
+  for i in range(0,test_cases):
+      n = int(input())
+      for i in range(0,n):
+              if i!=0:
                 if i%2!=0:
-                    print(i*i,end=" ")
+                  print(i*i,end=" ")
+                  
                 else:
-                    print(2*i,end=" ")
-            else:
+                  print(2*i,end=" ")
+                    
+              else:
                 print(i+3,end=" ") 
-    print("\n")   
+      print("\n")   
