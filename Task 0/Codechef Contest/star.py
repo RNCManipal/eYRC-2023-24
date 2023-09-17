cf4a22c0df07c3fc655c43da035d423ff4f0c308
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-09-17 15:08:11 +0530

    Update star.py

--- Task 0/Codechef Contest/star.py
+++ Task 0/Codechef Contest/star.py
@@ -1,23 +1,35 @@
-
 '''
 # Team ID:          < 2414 >
 # Theme:            < Cosmo Logistic >
 # Author List:      < Aditya Wadgaonkar,Harikishanthini K,Sujan Adiga,Tanay Srivastava  >
 # Filename:         < star.py >
-# Functions:        < None >
+# Functions:        < print_pattern,nain >
 # Global variables: < None >
 '''
+def print_pattern(test_cases,list):
+  '''
+  Purpose:
+  ---
+  To print star pattern 
+    
+  Input Arguments:
+  ---
+  test_cases : int
+    Number of star Patterns.
 
-t=int(input())                          #Test case number(t)
-                                        
-list=[]                                 #List to store star number
-for i in range (t):                     #
-    list.append(int(input()))           #Input star values for each testcase into a list
-                                        
-#The section below can also be used as a function
-#T
-                                        
-for n in range (t):                     #select star value for coressponding testcase
+  list : list
+    List of maximum number of stars in each pattern. 
+    
+  Returns:
+  ---
+  None
+    
+  Example call:
+  ---
+  print_pattern(test_cases,list)
+  '''
+  
+  for n in range (test_cases):                     
     for a in range(list[n],0,-1):       #Decrement star value for selected testcase
         for s in range(1,a+1):          #
             if (s%5==0):                #
@@ -25,3 +37,26 @@ for n in range (t):                     #select star value for coressponding tes
             else:                       #
                 print('*', end="")      #Print '*'
         print(' ')                      #Print newline before decrement
+  
+
+
+
+# Function Name:    main (built in)
+#        Inputs:    Number of test cases and maximum number of stars in each pattern
+#       Outputs:    NONE
+#       Purpose:    To call print_pattern() function and perform further operations
+if __name__ == '__main__':
+  test_cases=int(input())                          
+  #test_cases : Number of test cases
+                                        
+  list=[]                                
+  #list : List to store star number
+  
+  for i in range (test_cases):                     
+    list.append(int(input()))         
+  #Input star values for each testcase into a list
+  print_pattern(test_cases,list)
+  
+  
+                                        
+  
