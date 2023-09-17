a564ae65631bb3591ceef1a994b09f2d92ad66dd
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-09-17 13:38:42 +0530

    Update countChars.py

--- Task 0/Codechef Contest/countChars.py
+++ Task 0/Codechef Contest/countChars.py
@@ -3,47 +3,55 @@
 # Theme:            < Cosmo Logistic >
 # Author List:      < Aditya Wadgaonkar,Harikishanthini K,Sujan Adiga,Tanay Srivastava  >
 # Filename:         < countChars.py >
-# Functions:        < count_letters >
+# Functions:        < count_letters,main >
 # Global variables: < None >
 '''
 
-test_cases=int(input())
-l1=[]
-for j in range(0,test_cases):
-    str = input()
-    l1.append(str)
 
 
 def count_letters(str):
-    '''
-Purpose:
----
-< count letters of each word in a line begining  with '@' >
+  '''
+  Purpose:
+  ---
+  < count letters of each word in a line begining  with '@' >
 
-Input Arguments:
----
-`< str >` :  [ < string > ]
-    < A line string  >
+  Input Arguments:
+  ---
+  `< str >` :  [ < string > ]
+  < A line string  >
 
-Returns:
----
-`< None >` :  [ < None > ]
-    < None >
+  Returns:
+  ---
+  `< None >` :  [ < None > ]
+  < None >
 
-Example call:
----
-< count_letters(line) >
-'''
+  Example call:
+  ---
+  < count_letters(line) >
+  ''' 
+  lst=str.split()
+  for i in range(0,len(lst)):
+      if i==0:
+          print(len(lst[i])-1,end=",")
+      elif i == len(lst)-1:
+          print(len(lst[i]))
+      else:
+          print(len(lst[i]),end=",")
 
-    
-    lst=str.split()
-    for i in range(0,len(lst)):
-        if i==0:
-            print(len(lst[i])-1,end=",")
-        elif i == len(lst)-1:
-            print(len(lst[i]))
-        else:
-            print(len(lst[i]),end=",")
 
-for item in l1:
+
+# Function Name:    main (built in)
+#        Inputs:    Number of test cases and strings
+#       Outputs:    None
+#       Purpose:    To call the count_letters() function to perform further operations.
+if __name__ == "__main__":
+  test_cases=int(input())
+  l1=[]
+  for j in range(0,test_cases):
+    str = input()
+    l1.append(str)
+
+  for item in l1:
     count_letters(item)
+
+
